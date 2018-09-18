a0c9a1449401c729164adf0d184cc61ba46aff89
Linus <s3717624@student.rmit.edu.au>
2018-09-18 17:21:28 +1000

    Commit

---
+++ scrapesite/scrapesite/WebScraping.py
@@ -0,0 +1,30 @@
+#!/bin/env python3
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+def scrapefunction(userin, passin):
+    browser = webdriver.Chrome(executable_path='/home/skvutte/Downloads/chromedriver/chromedriver')
+    browser.set_window_size(1366,768)
+    browser.set_window_position(0,0)
+    url = "https://sso-cas.rmit.edu.au/rmitcas/login?service=https://mytimetable.rmit.edu.au/even/student"
+    browser.get(url)
+
+    username = browser.find_element_by_id("username")
+    password = browser.find_element_by_id("password")
+
+    username.send_keys(userin)
+    password.send_keys(passin)
+
+    time.sleep(15)
+
+    submit = browser.find_element_by_name("submit".click())
+
+
+
+    time.sleep(15)
+    html = browser.page_source
+    soup = BeautifulSoup(html, "lxml")
+    return soup.find_all('a')
+
--- scrapesite/scrapesite/views.py
+++ scrapesite/scrapesite/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import NameForm
+from .WebScraping import scrapefunction
+
+import sys
+
+from subprocess import run, PIPE
+
 
 def home(request):
     return render(request, "home.html")
@@ -9,9 +15,20 @@ def user(request):
     if request.method == 'POST':
         form = NameForm(request.POST)
         if form.is_valid():
-            userin = form.cleaned_data['username']
-            print(userin)
-            return render(request, "user.html", {'userin': userin})
+            userin = form.cleaned_data["username"]
+            passin = form.cleaned_data["password"]
+
+            scrapefunction(userin, passin)
+
+
+            #p = run(["WebScraping.py", userin, password], stdout=PIPE, input=None, encoding='utf-8')
+            #p = run(["/home/skvutte/PycharmProjects/WebScrape/scrapesite/WebScraping.py"], stdout=PIPE, input=None, encoding='utf-8')
+
+            #if p.returncode != 0: raise Exception
+
+            #print(p.stdout)
+            #return p.stdout
+
         else:
             return render(request, "home.html")
 
