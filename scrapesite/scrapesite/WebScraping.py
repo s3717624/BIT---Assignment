1997eb37a75da80fb9fe473575e6e46788215ec4
Linus <s3717624@student.rmit.edu.au>
2018-09-18 18:22:54 +1000

    Commit

--- scrapesite/scrapesite/WebScraping.py
+++ scrapesite/scrapesite/WebScraping.py
@@ -5,24 +5,26 @@ from selenium import webdriver
 import time
 
 def scrapefunction(userin, passin):
-    browser = webdriver.Chrome(executable_path='/home/skvutte/Downloads/chromedriver/chromedriver')
-    browser.set_window_size(1366,768)
-    browser.set_window_position(0,0)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('headless')
+    driver = webdriver.Chrome(executable_path="/home/skvutte/Downloads/chromedriver/chromedriver", chrome_options=chrome_options)
+    driver.set_window_size(1366,768)
+    driver.set_window_position(0,0)
     url = "https://sso-cas.rmit.edu.au/rmitcas/login?service=https://mytimetable.rmit.edu.au/even/student"
-    browser.get(url)
+    driver.get(url)
 
-    username = browser.find_element_by_id("username")
-    password = browser.find_element_by_id("password")
+    username = driver.find_element_by_id("username")
+    password = driver.find_element_by_id("password")
 
     username.send_keys(userin)
     password.send_keys(passin)
 
-    select = browser.find_element_by_name("submit")
+    select = driver.find_element_by_name("submit")
     if select:
         select.click()
 
-    #time.sleep(15)
-    #html = browser.page_source
-    #soup = BeautifulSoup(html, "lxml")
-    #return soup.find_all('a')
+    time.sleep(5)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "lxml")
+    return soup.find_all('a')
 
